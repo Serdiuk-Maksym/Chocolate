0eb2baa869a51c967d5f402aea4119eb8f735512
Serdiuk-Maksym <serdyukmaksimaleksandrovich@gmail.com>
2023-03-20 18:26:04 +0200

    12233333

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,3 @@
-@import url('https://code.jquery.com/jquery-3.6.4.min.js')
-@import url('./slick.min.js')
-@import url('./slider.js')
+import 'https://code.jquery.com/jquery-3.6.4.min.js';
+import './slick.min.js';
+import './slider.js';
