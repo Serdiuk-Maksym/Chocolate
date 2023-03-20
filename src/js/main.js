c465b3b342323da9fa8b67be1b20fd29f2bdfb02
Serdiuk-Maksym <serdyukmaksimaleksandrovich@gmail.com>
2023-03-20 18:50:20 +0200

    54364643

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,8 @@
-// Створення елементу <script> для підключення зовнішнього скрипту
-var externalScript = document.createElement('script');
-externalScript.src = 'https://code.jquery.com/jquery-3.6.4.min.js';
-document.head.appendChild(externalScript);
-
-// Підключення скрипту 1
-var script1 = document.createElement('script');
-script1.src = './slick.min.js';
-document.head.appendChild(script1);
-
-// Підключення скрипту 2
-var script2 = document.createElement('script');
-script2.src = './slider.js';
-document.head.appendChild(script2);
-
-// тут можна продовжити підключати інші скрипти, якщо вони є
+import 'https://code.jquery.com/jquery-3.6.4.min.js';
+import 'https://code.jquery.com/jquery-migrate-1.2.1.min.js';
+import 'https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js';
+import './slider';
+import './slider-reviews.js';
+import './modal.js';
+import './mobile-menu.js';
+import './no-active-link.js';
