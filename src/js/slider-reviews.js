aaf60c14c8e75d14783cc59959d2329a358c7543
Serdiuk-Maksym <serdyukmaksimaleksandrovich@gmail.com>
2023-03-20 18:10:01 +0200

    up reviews

--- src/js/slider-reviews.js
+++ src/js/slider-reviews.js
@@ -1,29 +1,34 @@
 $(document).ready(function () {
-    $('.reviews-list').slick({
-        arrows: false,
-        dots: true,
-        adaptiveHeight: true,
-        slidesToShow: 3,
-        speed: 1000,
-        easing: 'linear',
-        autoplay: true,
-        autoplaySpeed: 3000,
-        variableWidth: true,
-        responsive: [
-            {
-                breakpoint: 375,
-                settings: {slidesToShow: 1,}
-            },
-            {
-                breakpoint: 768,
-                settings: { slidesToShow: 2, }
-            },   
-            {
-               breakpoint: 1200,
-                settings: {slidesToShow: 3,} 
-            }
-        ],
-    });
+  $('.reviews-list').slick({
+    arrows: false,
+    dots: true,
+    adaptiveHeight: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    speed: 1000,
+    easing: 'linear',
+    infinite: true,
+    initialSlide: 0,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    pauseOnFocus: true,
+    pauseOnHover: true,
+    pauseOnDotsHover: true,
+    waitForAnimate: false,
+    variableWidth: true,
+    responsive: [
+      {
+        breakpoint: 375,
+        settings: { slidesToShow: 1 },
+      },
+      {
+        breakpoint: 768,
+        settings: { slidesToShow: 2 },
+      },
+      {
+        breakpoint: 1200,
+        settings: { slidesToShow: 3 },
+      },
+    ],
+  });
 });
-
- 
